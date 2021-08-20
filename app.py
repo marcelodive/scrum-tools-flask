663a62169e939674afeab7e9b0a53d2dd16b96dd
Marcelo <marcelodive@hotmail.com>
2021-08-20 17:06:08 -0300

    Spriting on rooms

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room
 from flask import jsonify
 
 app = Flask(__name__)
@@ -9,13 +9,16 @@ socketio.run(app)
 
 @socketio.on('room updated')
 def message(room):
-    socketio.emit('room updated', room)
+    socketio.emit('room updated', room, to = room['id'])
     print(room)
 
 @socketio.on('new user enter poker room')
 def message(newUserInfo):
-    socketio.emit('new user enter poker room', newUserInfo)
-    print(newUserInfo)
+    socketio.emit('new user enter poker room', newUserInfo, to = newUserInfo['roomId'])
+
+@socketio.on('join room')
+def on_join(roomId):
+    join_room(roomId)
 
 # @app.route("/")
 # def hello_world():
