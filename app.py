eedb97e0a1aa6e47cb63a6162ac3342fab61961b
Marcelo <marcelodive@hotmail.com>
2021-08-22 08:00:31 -0300

    ballot almost working

--- app.py
+++ app.py
@@ -9,15 +9,22 @@ socketio.run(app)
 
 @socketio.on('room updated')
 def message(room):
+    print('room updated')
     socketio.emit('room updated', room, to = room['id'])
-    print(room)
 
 @socketio.on('new user enter poker room')
 def message(newUserInfo):
+    print('new user enter poker room')
     socketio.emit('new user enter poker room', newUserInfo, to = newUserInfo['roomId'])
+
+@socketio.on('new user enter ballot room')
+def message(newUserInfo):
+    print('new user enter ballot room')
+    socketio.emit('new user enter ballot room', newUserInfo, to = newUserInfo['roomId'])
 
 @socketio.on('join room')
 def on_join(roomId):
+    print('join room')
     join_room(roomId)
 
 # @app.route("/")
@@ -28,4 +35,4 @@ def on_join(roomId):
 # def create_room():
 #     return jsonify('ckaei93')
 
-    # EC67pEiPD67RF4h
+# EC67pEiPD67RF4h
