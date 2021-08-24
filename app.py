16dc1f6a71666019cedd2d77012b0413ddb55516
Marcelo <marcelodive@hotmail.com>
2021-08-24 19:02:49 -0300

    Removing prints

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ socketio.run(app)
 
 @app.route('/<path:path>', methods=['GET'])
 def static_proxy(path):
-  print(path)
   return send_from_directory('./', path)
 
 @app.route('/')
@@ -21,27 +20,22 @@ def root_en():
 
 @socketio.on('room updated')
 def room_updated(room):
-    print('room updated')
     socketio.emit('room updated', room, to = room['id'])
 
 @socketio.on('new user enter poker room')
 def new_user_poker(newUserInfo):
-    print('new user enter poker room')
     socketio.emit('new user enter poker room', newUserInfo, to = newUserInfo['roomId'])
 
 @socketio.on('new user enter ballot room')
 def new_user_ballot(newUserInfo):
-    print('new user enter ballot room')
     socketio.emit('new user enter ballot room', newUserInfo, to = newUserInfo['roomId'])
 
 @socketio.on('join room')
 def on_join(roomId):
-    print('join room')
     join_room(roomId)
 
 @socketio.on('left room')
 def on_left(userInfo):
-    print('user left')
     leave_room(userInfo['roomId'])
     socketio.emit('user left the room', userInfo['username'], to = userInfo['roomId'])
     
