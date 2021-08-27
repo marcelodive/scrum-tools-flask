4da258b5b2423065c6d685748ad422e4024d6243
Marcelo <marcelodive@hotmail.com>
2021-08-27 01:35:47 -0300

    deploy.sh

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/sammy/scrum/venv"
+set -gx VIRTUAL_ENV "/home/marcelo/Development/scrum-tools/scrum-tools-flask/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
